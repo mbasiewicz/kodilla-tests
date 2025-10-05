9f041f1eed3ec2ac58e847fb57bd0b2a1c6d6985
Marcin Basiewicz <m.basiewicz@tme.pl>
2025-10-05 12:24:28 +0200

    pełna funkcja dzieląca na czynniki pierwsze i zapisująca do listy

--- prime_factors.py
+++ prime_factors.py
@@ -1,2 +1,17 @@
 def prime_factors_result(number):
-    return None
+    """funkcja rozkłada liczbę na czynniki pierwsze:
+    zwraca liczbę pierwszą lub jej czynniki w tabeli"""
+    factors = []
+    divisor = 2
+
+    while number != 1:
+        if number % divisor == 0:
+            factors.append(divisor)
+            number = number // divisor
+        else:
+            divisor += 1
+
+    return factors
+
+
+print(prime_factors_result(21))
